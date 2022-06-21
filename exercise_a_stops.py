9567a788f9507493aae0b5d0719ba45156a3fe85
Samuel <samuelsf@hotmail.co.uk>
2022-06-21 16:27:36 +0100

    finished 4a

--- exercise_a_stops.py
+++ exercise_a_stops.py
@@ -1,3 +1,6 @@
+from operator import indexOf
+
+
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 
 #1. Add "Edinburgh Waverley" to the end of the list
@@ -7,7 +10,7 @@ stops.insert(0, "Glasgow Queen St")
 #3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
 stops.insert(4,"Polmont")
 #4. Print out the index position of "Linlithgow"
-print(stops["Linlithgow"])
+print(indexOf(stops, "Linlithgow"))
 #5. Remove "Livingston" from the list using its name
 #6. Delete "Cumbernauld" from the list by index
 #7. Print the number of stops there are in the list
